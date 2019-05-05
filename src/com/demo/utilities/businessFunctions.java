e8fcb811a0a1db9a3abb176b0a1f80f1736d9d55
CodeWidSid <sid.deagle@gmail.com>
2019-05-05 18:04:55 +0530

    Added Function Library and Business functions v1

--- src/com/demo/utilities/businessFunctions.java
+++ src/com/demo/utilities/businessFunctions.java
@@ -1,13 +1,21 @@
 package com.demo.utilities;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import com.demo.utilities.*;
 
 public class businessFunctions extends commonUtils{
 	
-	
 	@Test
-	public void Login() {
-		
+	public void login() throws FileNotFoundException, IOException {
+		clearCache("chrome");
+		setBrowser("chrome");
+		navigate("http://demo.guru99.com/v4/index.php");
+		DFsendkeys(getObject("loginPage_Username_Input"),"mngr192887");
+		DFsendkeys(getObject("loginPage_Password_Input"), "YtysAmu");
+		click(getObject("loginPage_LoginButton_input"));
 	}
 }
--- src/com/demo/utilities/commonUtils.java
+++ src/com/demo/utilities/commonUtils.java
@@ -1,44 +1,168 @@
 package com.demo.utilities;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 
 public class commonUtils{
-	static String path = System.getProperty("user.dir");
-	static String ScreenshotDir = path+"\\screenshots";
-	static String ORPath = path+"\\objects\\objectRepo.properties";
-	static String WebDriverPath = path+"\\driver\\chromedriver.exe";
-	WebDriver driver;
+	public static String basePath = System.getProperty("user.dir");
+	public static String configPath = basePath+"\\config\\config.properties";
+	public static String driversPath = basePath+"\\drivers";
+	public static String objectRepo = basePath+"\\objectRepo.properties";
+	public static String reportsPath = basePath+"\\reports";
+	public static String sreenshotsPath = basePath+"\\screenshots";
+	public static String testdataPath = basePath+"\\testdata";
+	public static String scriptPath = basePath+"\\scripts";
+	public WebDriver driver;
 	
-	@Test
-	public void test1() throws IOException {
-		driver.findElement(By.id(getObject("loginPage_Password_Input"))).click();
+	/************************************************************
+	 * Description : Loads global configuration parameters 
+	 * from config.properties file
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 ***********************************************************/
+	public void loadConfig(String path,String key) throws FileNotFoundException, IOException {
+		String scrShotPassed = null;
+		Properties properties = new Properties();
+		properties.load(readFile(path));
+		properties.getProperty(key);
 	}
 	
-	public static void takeScreenPrint(String result) {
-		if(result.equalsIgnoreCase("Fail")) {
-			System.out.println("saved in location "+path+"\\screenshots\\"+result+".png");
+	/***********************************************************
+	 * Description :  Reads file contents of the path specified
+	 * and makes available to the user
+	 * @throws FileNotFoundException 
+	 **********************************************************/
+	public FileInputStream readFile(String path) throws FileNotFoundException {
+		FileInputStream fis = new FileInputStream(path);
+		return fis;
+	}
+	
+	/************************************************************
+	 * Description : Takes browser type (ie,chrome etc) as 
+	 * argument and clears cache and cookies from commandline
+	 ***********************************************************/
+	
+	public void clearCache(String browser) throws IOException {
+		Process run;
+		if(browser.equalsIgnoreCase("ie")||browser.equalsIgnoreCase("InternetExplorer")) {
+			run = Runtime.getRuntime().exec("cmd.exe /c Start "+scriptPath+"\\IECacheClear.bat");
+		}else if(browser.equalsIgnoreCase("gc")||browser.equalsIgnoreCase("chrome")||
+				browser.equalsIgnoreCase("googlechrome")) {
+			run = Runtime.getRuntime().exec("cmd.exe /c Start "+scriptPath+"\\chromeClearCache.bat");
+		}else if(browser.equalsIgnoreCase("ff")||browser.equalsIgnoreCase("firefox")){
+			run = Runtime.getRuntime().exec("cmd.exe /c Start "+scriptPath+"\\firefoxClearCache.bat");
+		}else {
+			System.out.println("ERROR : BROWSER NOT SUPPORTED BY FRAMEWORK");
 		}
+		
 	}
 	
-	public static void click(WebElement element) {
+	
+	/***********************************************************
+	 * Description : Quits active browser and closes session
+	 * and webdriver server instance
+	 **********************************************************/
+	@AfterTest
+	public void quitSession() {
+		driver.close();
+		driver.quit();
+	}
+	
+	/***********************************************************
+	 * Description : Return a webelement from Object repository
+	 * with appropriate selector based on key-value pairs
+	 **********************************************************/
+	public WebElement getObject(String Key) throws FileNotFoundException, IOException {
+		WebElement element;
+		Properties properties = new Properties();
+		properties.load(readFile(objectRepo));
+		String LocatorType;
+		String Locator;
+		LocatorType = properties.getProperty(Key).split(":")[0].toLowerCase();
+		Locator = properties.getProperty(Key).split(":")[1];
+		switch (LocatorType) {
+			case "name":
+				element =  driver.findElement(By.name(Locator));
+				break;
+			case "id":
+				element =  driver.findElement(By.id(Locator));
+				break;
+			case "class":
+				element =  driver.findElement(By.className(Locator));
+				break;
+			case "linktext":
+				element =  driver.findElement(By.linkText(Locator));
+				break;
+			case "partiallinktext":
+				element =  driver.findElement(By.partialLinkText(Locator));
+				break;
+			case "xpath":
+				element =  driver.findElement(By.xpath(Locator));
+				break;
+			case "tagname":
+				element =  driver.findElement(By.tagName(Locator));
+				break;
+				default:
+					return null;
+		}
+		return element;
+	}
+	
+	/***************************************************************************
+	 * Description : Clicks on specified webelement provided as argument
+	 * within the function.
+	 **************************************************************************/
+	public void click(WebElement element) {
 		element.click();
 	}
 	
-	public static void sendKeys(WebElement element,String data) {
-		element.sendKeys(data);
+	/****************************************************************************
+	 * Description : Sends character sequence within a webelement
+	 * provided as an argument within the function. 
+	 ***************************************************************************/
+	public void DFsendkeys(WebElement element,String inputdata) {
+		element.sendKeys(inputdata);
 	}
 	
-	public static String getObject(String Key) throws IOException {
-		FileInputStream fis = new FileInputStream(ORPath);
-		Properties properties = new Properties();
-		properties.load(fis);
-		return Key;
+	/**********************************************************
+	 * Description : creates an instance of a Webdriver server
+	 * based on user's input parameter (ie,chrome,ff etc)
+	 *********************************************************/
+	public void setBrowser(String browserName) {
+		if(browserName.equalsIgnoreCase("ie")||(browserName.equalsIgnoreCase("internetExplorer"))) {
+			System.setProperty("webdriver.ie.driver", driversPath+"\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}else if(browserName.equalsIgnoreCase("gc")||(browserName.equalsIgnoreCase("chrome"))||
+				browserName.equalsIgnoreCase("googlechrome")) {
+			System.setProperty("webdriver.chrome.driver", driversPath+"\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}else if (browserName.equalsIgnoreCase("ff")||browserName.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", driversPath+"\\geckoDriver.exe");
+			driver = new FirefoxDriver();
+		}else {
+			System.out.println("ERROR : BROWSER NOT SUPPORTED BY FRAMEWORK");
+		}
 	}
+	
+	/*********************************************************
+	 * Description : Navigates to provided url in the argument
+	 * within function
+	 ********************************************************/
+	public void navigate(String url) {
+		driver.manage().window().maximize();
+		driver.get(url);
+	}
+	
 }
